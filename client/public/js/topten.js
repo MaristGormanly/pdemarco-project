56452fd056091c6e135c149c8909a88f126ca9be
Preston Mac <prestondemarco@148-100-151-44.FoxNet.marist.edu>
2018-03-07 14:24:14 -0500

    project

--- client/public/js/topten.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$(document).keydown(function(e) {
-      if (e.keyCode == '32') {
-        window.location.href = 'http://localhost:1337/mainmenu';
-      }
-    });
-  });
